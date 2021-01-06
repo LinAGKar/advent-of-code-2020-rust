74118b1b9679ffe37ff40ac4f1613114c2ed3674
Linus Kardell <linus.kardell@gmail.com>
2021-01-06 20:00:44 +0100

    Faster day 15

--- day15b/src/main.rs
+++ day15b/src/main.rs
@@ -4,27 +4,26 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 
-    const ITERATIONS: usize = 30000000;
+    const ITERATIONS: i32 = 30000000;
 
     let numbers: Vec<_> = input.trim().split(',').map(|x| x.parse().unwrap()).collect();
-    let (&last_number, rest) = numbers.split_last().unwrap();
-    let mut number_timestamps = vec![None; ITERATIONS];
+    let (&last_number, rest): (&i32, _) = numbers.split_last().unwrap();
+    let mut last_number = last_number;
+    let mut number_timestamps = vec![-1; ITERATIONS as usize];
     for (&i, n) in rest.iter().zip(1..) {
-        number_timestamps[i] = Some(n);
+        number_timestamps[i as usize] = n;
     }
-    
-    let (_, last_number) = (numbers.len()..ITERATIONS).fold(
-        (number_timestamps, last_number),
-        |(mut number_timestamps, last_number), curr_time| {
-            let new_number = if let Some(last_time) = number_timestamps[last_number] {
-                curr_time - last_time
-            } else {
-                0
-            };
-            number_timestamps[last_number] = Some(curr_time);
-            (number_timestamps, new_number)
-        },
-    );
+
+    for curr_time in numbers.len() as i32..ITERATIONS {
+        let last_time = number_timestamps[last_number as usize];
+        let new_number = if last_time >= 0 {
+            curr_time - last_time
+        } else {
+            0
+        };
+        number_timestamps[last_number as usize] = curr_time;
+        last_number = new_number;
+    }
 
     println!("{}", last_number);
 }
